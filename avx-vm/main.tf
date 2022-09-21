12052121e399dab9de786f12ef3925e29231d4fb
Alex Weiss <alexandreweiss@hotmail.com>
2022-09-21 15:12:33 +0200

    update for avx2

---
+++ avx-vm/main.tf
@@ -0,0 +1,71 @@
+resource "oci_core_vcn" "default_oci_core_vcn" {
+  cidr_block     = var.oci_core_vcn_cidr
+  compartment_id = var.compartment_ocid
+  display_name   = "vm-vcn"
+  dns_label      = var.oci_core_vcn_dns_label
+}
+
+resource "oci_core_subnet" "default_oci_core_subnet" {
+  cidr_block        = var.oci_core_subnet_cidr
+  compartment_id    = var.compartment_ocid
+  display_name      = "${var.oci_core_subnet_cidr} vm-vcn-subnet"
+  dns_label         = var.oci_core_subnet_dns_label
+  route_table_id    = oci_core_vcn.default_oci_core_vcn.default_route_table_id
+  vcn_id            = oci_core_vcn.default_oci_core_vcn.id
+  security_list_ids = [oci_core_default_security_list.default_security_list.id]
+}
+
+resource "oci_core_instance" "ubuntu_oci_instance" {
+  agent_config {
+    is_management_disabled = "false"
+    is_monitoring_disabled = "false"
+
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Vulnerability Scanning"
+    }
+
+    plugins_config {
+      desired_state = "DISABLED"
+      name          = "Bastion"
+    }
+  }
+
+  availability_config {
+    recovery_action = "RESTORE_INSTANCE"
+  }
+
+  availability_domain = var.availability_domain
+  compartment_id      = var.compartment_ocid
+  fault_domain        = var.default_fault_domain
+
+  create_vnic_details {
+    assign_private_dns_record = true
+    assign_public_ip          = var.is_private == true ? false : true
+    subnet_id                 = var.is_private == true ? var.private_subnet_id : var.public_subnet_id
+  }
+
+  display_name = "nginx-vm"
+
+  instance_options {
+    are_legacy_imds_endpoints_disabled = false
+  }
+
+  is_pv_encryption_in_transit_enabled = true
+
+  metadata = {
+    "ssh_authorized_keys" = file(var.PATH_TO_PUBLIC_KEY)
+    "user_data"           = data.template_cloudinit_config.ubuntu_init.rendered
+  }
+
+  shape = "VM.Standard.E3.Flex"
+  shape_config {
+    memory_in_gbs = "2"
+    ocpus         = "1"
+  }
+
+  source_details {
+    source_id   = var.os_image_id
+    source_type = "image"
+  }
+}
---
+++ avx-vm/variables.tf
@@ -0,0 +1,66 @@
+variable "compartment_ocid" {
+  default = "ocid1.compartment.oc1..aaaaaaaa2ph2o3lwq5lq6qdsxdub4jqum25fj6h3uv3v5vxipjcdxkh2xhcq"
+}
+
+variable "region" {
+  default = "eu-marseille-1"
+}
+
+variable "fault_domains" {
+  type    = list(any)
+  default = ["FAULT-DOMAIN-1", "FAULT-DOMAIN-2", "FAULT-DOMAIN-3"]
+}
+
+variable "oci_core_vcn_cidr" {
+  default = "10.0.0.0/24"
+}
+
+variable "oci_core_subnet_cidr" {
+  default = "10.0.0.0/28"
+}
+
+variable "oci_core_vcn_dns_label" {
+  default = "defaultvcn"
+}
+
+variable "oci_core_subnet_dns_label" {
+  default = "publicsubnet10"
+}
+
+variable "my_public_ip_cidr" {
+  type        = string
+  description = "My public ip CIDR"
+  default = "0.0.0.0/0"
+}
+
+variable "availability_domain" {
+  default = "eu-marseille-1-ad-1"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  type        = string
+  default     = "../../ssh/oci-id_key.pem"
+  description = "OCI public key"
+}
+
+variable "is_private" {
+  type    = bool
+  default = false
+}
+
+variable "public_subnet_id" {
+  type = string
+  default = "ocid1.subnet.oc1.eu-marseille-1.aaaaaaaaf5behhm3aen37ysp6ty4mia52bxdekp4cy6ru7f2zyd6ygnonzfq"
+}
+
+variable "private_subnet_id" {
+  type = string
+}
+
+variable "default_fault_domain" {
+  default = "FAULT-DOMAIN-1"
+}
+
+variable "os_image_id" {
+  default = "ocid1.image.oc1.eu-zurich-1.aaaaaaaag2uyozo7266bmg26j5ixvi42jhaujso2pddpsigtib6vfnqy5f6q" # Canonical-Ubuntu-20.04-aarch64-2022.01.18-0
+}
